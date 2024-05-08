218592c61dabf74b588b237132e80262e2b006f9
IanSantosOS <iann.silva2006@gmail.com>
2024-05-08 11:50:38 -0300

    more telescope functionality

--- lua/desktop/plugins/telescope.lua
+++ lua/desktop/plugins/telescope.lua
@@ -4,7 +4,15 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         local builtin = require('telescope.builtin')
+        local dropdown = require('telescope.themes').get_dropdown({})
+
+        vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
+
+        vim.keymap.set('n', '<leader>pc', function() builtin.colorscheme(dropdown) end, {}) -- change keybind
+        vim.keymap.set('n', '<leader>h', builtin.treesitter, {}) -- change keybind
     end
 }

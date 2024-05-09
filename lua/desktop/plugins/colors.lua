3d74050c9132c1e7a1dcb0c9256598c0b3326dc0
Ian Santos <iann.silva2006@gmail.com>
2024-05-08 22:34:57 -0300

    change in colors + ignoring lazy-lock.json

--- lua/desktop/plugins/colors.lua
+++ lua/desktop/plugins/colors.lua
@@ -15,7 +15,7 @@ return {
                 light = "latte",
                 dark = "mocha",
             },
-            transparent_background = true, -- disables setting the background color.
+            transparent_background = false, -- disables setting the background color.
             show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
             term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
             dim_inactive = {
@@ -26,41 +26,6 @@ return {
             no_italic = true, -- Force no italic
             no_bold = false, -- Force no bold
             no_underline = false, -- Force no underline
-            styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-                comments = { "italic" }, -- Change the style of comments
-                conditionals = { "italic" },
-                loops = {},
-                functions = {},
-                keywords = {},
-                strings = {},
-                variables = {},
-                numbers = {},
-                booleans = {},
-                properties = {},
-                types = {},
-                operators = {},
-                -- miscs = {}, -- Uncomment to turn off hard-coded styles
-            },
-            color_overrides = {},
-            custom_highlights = {},
-            -- default_integrations = true,
-            integrations = {
-                cmp = true,
-                gitsigns = true,
-                notify = false,
-                nvimtree = true,
-                treesitter = true,
-                harpoon = true,
-                telescope = {
-                    enabled = true,
-                    style = "nvchad",
-                },
-                mini = {
-                    enabled = true,
-                    indentscope_color = "",
-                },
-                -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-            },
         })
 
         vim.cmd('colorscheme catppuccin')

97fb84f4d8c68b1c55a84be569fee51ba312d8ca
IanSantosOS <iann.silva2006@gmail.com>
2024-05-21 09:16:27 -0300

    comments

--- lua/iansantosos/vimset.lua
+++ lua/iansantosos/vimset.lua
@@ -15,7 +15,7 @@ end
 vim.opt.compatible = false        -- Desative o modo de compatibilidade com o VI
 
 vim.opt.termguicolors = true      -- Permite o uso de cores 24-bit
-vim.opt.updatetime = 50           --
+vim.opt.updatetime = 50           -- Define o intervalo (ms) para ações automáticas (ex.: plugins, eventos CursorHold)
 
 vim.g.netrw_banner = 0            -- Desativa o banner do netrw
 vim.g.netrw_altv = 1              -- Arquivos abertos usando "v" irão abrir a direita do netrw
@@ -32,9 +32,9 @@ vim.opt.relativenumber = true     -- Ativa o relative number
 vim.opt.signcolumn = "yes"        -- Sempre exibir um espaço do lado esquerdo da coluna de números para marcadores visuais
 -- vim.opt.colorcolumn = '80'     -- Uma coluna visual para delimitar a quantidade de caracteres
 
-vim.opt.wrap = false              --
-vim.opt.scrolloff = 8             --
-vim.opt.sidescrolloff = 8         --
+vim.opt.wrap = false              -- Desativa a quebra de linha automática
+vim.opt.scrolloff = 8             -- Distância para ativar a rolagem vertical do texto
+vim.opt.sidescrolloff = 8         -- Distância para ativar a rolagem horizontal do texto
 
 vim.opt.tabstop = 4               -- Define que 1 Tab = 4 Espaços
 vim.opt.softtabstop = 0           -- Os espaços inseridos quando aperta Tab ou BaskSpace será o mesmo do tabstop
@@ -42,9 +42,9 @@ vim.opt.shiftwidth = 4            -- Espaços inseridos ao utilizar a tecla de r
 vim.opt.expandtab = true          -- Transforma Tab em Espaços
 vim.opt.smartindent = true        -- Indentação inteligente
 
-vim.opt.smartcase = true          --
-vim.opt.ignorecase = true         --
-vim.opt.incsearch = true          --
+vim.opt.smartcase = true          -- Respeita maiúsculas/minúsculas se houver letras maiúsculas na busca.
+vim.opt.ignorecase = true         -- Ignora maiúsculas/minúsculas em buscas
+vim.opt.incsearch = true          -- Realiza as buscas durante a digitação
 
 vim.opt.showcmd = true            -- Mostrar a linha de comando (":q" por exemplo)
 vim.opt.cmdheight = 1             -- Tamanho da linha de comando
@@ -54,16 +54,18 @@ vim.opt.laststatus = 2            -- Sempre mostrar a linha de status
 
 vim.o.wildmenu = true             -- Mostrar um menu de opções na linha de comando quando clicar Tab
 
-vim.opt.swapfile = false          --
-vim.opt.backup = false            --
-vim.opt.undofile = true           --
+vim.opt.swapfile = false          -- Desativa a criação de arquivos de troca para recuperação automática em caso de falha ou fechamento inesperado.
+vim.opt.backup = false            -- Desativa a criação de cópias de segurança dos arquivos antes de serem salvos.
+vim.opt.undofile = true           -- Habilita a gravação de histórico de edições em arquivo para recuperação entre sessões.
 
--- Decide onde colocará a pasta de undodir
+
+-- Decide onde colocará a pasta de undodir que foi habilitada pelo "undofile"
 if vim.fn.has("win32") == 1 then
     vim.opt.undodir = vim.fn.expand("$HOME") .. "/AppData/Local/nvim/undodir"
 else
     vim.opt.undodir = vim.fn.expand("$HOME") .. "/.vim/undodir"
 end
+
 
 -- Mudar o filetype do EJS para HTML
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
@@ -72,6 +74,7 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
         vim.bo.filetype = "html"
     end,
 })
+
 
 -- Highlight quando está copiando (yanking) o texto
 vim.api.nvim_create_autocmd("TextYankPost", {
